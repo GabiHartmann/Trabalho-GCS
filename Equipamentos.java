8349800b7e0df12783534fa76bbfc2f0b0b39d48
GabiHartmann <166546663+GabiHartmann@users.noreply.github.com>
2025-04-26 18:09:11 -0300

    adicao dos metodos getters e setter, comeco da classe App e classe de cadastro

--- Equipamentos.java
+++ Equipamentos.java
@@ -16,4 +16,73 @@ public class Equipamentos{
         this.funcionario = funcionario;
         this.tipo = tipo;
     }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }  
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getNmrManutencao() {
+        return nmrManutencao;
+    }
+
+    public void setNmrManutencao(int nmrManutencao) {
+        this.nmrManutencao = nmrManutencao;
+    }
+
+    public String getDataAquizicao() {
+        return dataAquizicao;
+    }
+
+    public void setDataAquizicao(String dataAquizicao) {
+        this.dataAquizicao = dataAquizicao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipamentos{" +
+                "identificador=" + identificador +
+                ", descricao='" + descricao + '\'' +
+                ", nmrManutencao=" + nmrManutencao +
+                ", dataAquizicao='" + dataAquizicao + '\'' +
+                ", valor=" + valor +
+                ", funcionario=" + funcionario +
+                ", tipo='" + tipo + '\'' +
+                '}';
+    }
 }
--- Funcionario.java
+++ Funcionario.java
@@ -8,4 +8,37 @@ public class Funcionario{
         this.email = email;
         this.matricula = matricula;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", matricula=" + matricula +
+                '}';
+    }
 }
